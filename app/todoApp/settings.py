ae3ef46ff9e7542b7418e1341402e0c5f022b791
Daniel Matthes <danielmatthes10@gmail.com>
2024-03-22 12:56:01 -0300

    .

--- app/todoApp/settings.py
+++ app/todoApp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(os.environ.get("DEBUG", default=0))
 
-if os.environ.get("DJANGO_ALLOWED_HOSTS").split(","):
+if os.environ.get("DJANGO_ALLOWED_HOSTS"):
     ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(",")
 else:
     ALLOWED_HOSTS = []
